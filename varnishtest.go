d4e9292da3a0340b75a8a4b1cf677628e80780ea
Guillaume Quintard <guillaume.quintard@varnish-software.com>
2025-09-15 17:50:17 -0700

    Varnish Cache compatibility

--- varnishtest.go
+++ varnishtest.go
@@ -1,13 +1,18 @@
 package varnishtest
 
 import (
-	"encoding/json"
+	"crypto/sha256"
+	"encoding/hex"
+	"path/filepath"
 	"fmt"
 	"log"
+	"net"
 	"net/http"
 	"net/url"
 	"os"
 	"os/exec"
+	"strings"
+	"time"
 
 	"github.com/google/uuid"
 )
@@ -39,6 +44,7 @@ type Varnish struct {
 	client *http.Client
 	cmd    *exec.Cmd
 	name   string
+	conn     net.Conn
 }
 
 type socket struct {
@@ -113,7 +119,12 @@ func (uv *UnstartedVarnish) Backend(name string, urlRaw string) *UnstartedVarnis
 }
 
 func (uv *UnstartedVarnish) Start() Varnish {
-	name := uuid.NewString()
+	sock, err := net.Listen("tcp", ":0")
+	if err != nil {
+		panic(err)
+	}
+
+	name := fmt.Sprintf("/tmp/varnishtest-go.%s", uuid.NewString())
 
 	args := []string{
 		"-F",
@@ -127,6 +138,7 @@ func (uv *UnstartedVarnish) Start() Varnish {
 		"-p", "vsl_mask=+Debug,+H2RxHdr,+H2RxBody",
 		"-p", "h2_initial_window_size=1m",
 		"-p", "h2_rx_window_low_water=64k",
+		"-M", sock.Addr().String(),
 	}
 	for _, p := range uv.parameters {
 		args = append(args, p.name, p.value)
@@ -136,22 +148,51 @@ func (uv *UnstartedVarnish) Start() Varnish {
 		args...,
 	)
 
-	varnish := Varnish{
-		cmd:  cmd,
-		name: name,
-	}
-
-	err := cmd.Start()
+	err = cmd.Start()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// wait for Varnish to come online
-	byt, err := varnish.Adm("ping")
+	// wait for the cli message
+	conn, err := sock.Accept()
 	if err != nil {
-		log.Fatalf("%s\n%s\n", string(byt), err)
+		panic(err)
 	}
 
+	varnish := Varnish{
+		cmd:  cmd,
+		name: name,
+		conn: conn,
+	}
+	status, nonce, err := varnish.readCliMessage()
+	if status != 107 {
+		panic("status should have been 107")
+	}
+	fmt.Printf("nonce is: %s\n", nonce)
+	if len(nonce) < 32 {
+		panic("nonce too short")
+	}
+
+	secret, err := os.ReadFile(filepath.Join(name, "_.secret"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	hasher := sha256.New()
+	hasher.Write(nonce[:32])
+	hasher.Write([]byte("\n"))
+	hasher.Write(secret)
+	hasher.Write(nonce[:32])
+	hasher.Write([]byte("\n"))
+
+	status, _, err = varnish.Adm("auth", hex.EncodeToString(hasher.Sum(nil)))
+	if status != 200 {
+		panic("status should have been 200")
+	}
+	if err != nil {
+		log.Fatal(err)
+        }
+
+	var byt []byte
 	if !uv.vclIsFile {
 		backendString := ""
 		for _, b := range uv.backends {
@@ -164,53 +205,93 @@ func (uv *UnstartedVarnish) Start() Varnish {
 		}
 
 		vcl := fmt.Sprintf("%s%s%s", uv.vclVersion, backendString, uv.vclString)
-		byt, err = varnish.Adm("vcl.inline", "vcl1 << XXYYZZ\n", vcl, "\nXXYYZZ")
+		_, byt, err = varnish.Adm("vcl.inline", "vcl1 << XXYYZZ\n", vcl, "\nXXYYZZ")
 		if err != nil {
 			log.Fatalf("%s\n%s\n", string(byt), err)
 		}
 	} else {
-		byt, err = varnish.Adm("vcl.load", "vcl1", uv.vclString)
+		_, byt, err = varnish.Adm("vcl.load", "vcl1", uv.vclString)
 		if err != nil {
 			log.Fatalf("%s\n%s\n", string(byt), err)
 		}
 	}
 
-	byt, err = varnish.Adm("vcl.use", "vcl1")
+	_, byt, err = varnish.Adm("vcl.use", "vcl1")
 	if err != nil {
 		log.Fatalf("%s\n%s\n", string(byt), err)
 	}
-	byt, err = varnish.Adm("start")
-	if err != nil {
-		log.Fatalf("%s\n%s\n", string(byt), err)
-	}
-
-	// find a port
-	byt, err = varnish.Adm("socket.list", "-j")
+	_, byt, err = varnish.Adm("start")
 	if err != nil {
 		log.Fatalf("%s\n%s\n", string(byt), err)
 	}
 
-	// one day, `varnishadm` will output proper JSON
-	var fullResponse any
-	err = json.Unmarshal(byt, &fullResponse)
-	if err != nil {
-		log.Fatal(err)
-	}
-	varnish.URL = fmt.Sprintf("http://%v", fullResponse.([]any)[3].([]any)[0].(map[string]any)["endpoint"])
+	varnish.WaitRunning()
 
 	return varnish
 }
 
-func (varnish *Varnish) Adm(args ...string) ([]byte, error) {
-	args = append([]string{"-n", varnish.name}, args...)
-	c := exec.Command("varnishadm", args...)
+func (varnish *Varnish) WaitRunning() error {
+	status, resp, err := varnish.Adm("status")
+	for {
+		if err != nil {
+			return err
+		}
+		if status != 200 {
+			return fmt.Errorf(`"status" request got a %d response:\n%s`, status, string(resp))
+		}
+		if string(resp) == "Child in state stopped" {
+			return fmt.Errorf("Child stopped before running")
+		}
+		if string(resp) == "Child in state running\n" {
+			status, resp, err = varnish.Adm("debug.listen_address")
+			if err != nil {
+				return err
+			}
 
-	byt, err := c.Output()
-	return byt, err
+			var name string
+			var addr string
+			var port int
+			_, err := fmt.Sscanf(string(resp), "%s %s %d\n", &name, &addr, &port)
+			if err != nil {
+				return err
+			}
+			// FIXME: IPv6
+			varnish.URL = fmt.Sprintf("http://%s:%d", addr, port)
+			break
+		}
+		time.Sleep(200 * time.Millisecond)
+	}
+	return nil
+}
+
+func (varnish *Varnish) readCliMessage() (int, []byte, error) {
+	status := 0
+	sz := 0
+	_, err := fmt.Fscanf(varnish.conn, "%d %d\n", &status, &sz)
+	if err != nil {
+		return 0, []byte{}, err
+	}
+	buf := make([]byte, sz + 1)
+
+	_, err = varnish.conn.Read(buf)
+	if err != nil {
+		return 0, []byte{}, err
+	}
+
+	return status, buf, nil
+}
+
+func (varnish *Varnish) Adm(args ...string) (int, []byte, error) {
+	_, err := varnish.conn.Write([]byte(strings.Join(args, " ") + "\n"))
+	if err != nil {
+		return 0, []byte{}, err
+	}
+	return varnish.readCliMessage()
 }
 
 func (varnish *Varnish) Close() {
 	varnish.Adm("stop")
+	varnish.conn.Close()
 
 	if err := varnish.cmd.Process.Kill(); err != nil {
 		log.Printf("failed to kill process: %s\n", err)
